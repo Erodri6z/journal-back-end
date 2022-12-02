84540bf8d8fde0f5c62bc4422a1d93395fd88a64
Erodri6z <erikrodriguez2417@gmail.com>
2022-12-01 23:48:05 -0600

    add checkauth to note routes

--- routes/notes.js
+++ routes/notes.js
@@ -11,9 +11,9 @@ router.get('/', notesCtrl.index)
 
 /*---------- Protected Routes ----------*/
 router.post('/',checkAuth, notesCtrl.create)
-router.get('/:id', notesCtrl.show)
-router.put('/:id', notesCtrl.update)
-router.delete('/:id', notesCtrl.delete)
+router.get('/:id',checkAuth, notesCtrl.show)
+router.put('/:id',checkAuth, notesCtrl.update)
+router.delete('/:id',checkAuth, notesCtrl.delete)
 router.use(decodeUserFromToken)
 
 export { router }
